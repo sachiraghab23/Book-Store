dd31ffafc29389e64e07496936c7334930609898
sachiraghab <sachiraghab@gmail.com>
2023-09-01 18:28:40 +0530

    server and database created and connection is set

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://sachiraghab:myBook23sde@cluster0.0rf1haf.mongodb.net/?retryWrites=true&w=majority'
---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+const app = express();
+app.get('/',(req,res)=>{
+  // console.log(req);
+  return res.status(234).send('Welcome to our Book Store');
+});
+mongoose.connect(mongoDBURL)
+.then(()=>{
+  console.log("App connected to database");
+  app.listen(PORT,()=>{
+    console.log(`App is listening to port: ${PORT}`);
+  });
+})
+.catch((err)=>{
+  console.log(err);
+});
