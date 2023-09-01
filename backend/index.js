4de10d0b17b76bf41a7641b3ccae09829aca17f0
sachiraghab <sachiraghab@gmail.com>
2023-09-02 00:28:22 +0530

    react frontend part setup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,32 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import bookRoute from './routes/bookRoutes.js';
+import cors from 'cors';
+
 const app = express();
+
+//middleware for parsing request body
+app.use(express.json());
+
+//middleware for handling CORS policy
+app.use(cors());
+/*
+option 1: Allow all origins with default of cors(*)
+  app.use(cors());
+option 2: Allow custom origins
+ app.use(
+  cors({
+    origin: 'http://localhost:3000,
+    methods: ['GET','POST','PUT','DELETE']
+    allowdHeaders: ['Content-Type'],
+  })
+ );
+*/
 app.get('/',(req,res)=>{
-  // console.log(req);
   return res.status(234).send('Welcome to our Book Store');
 });
+app.use('/books',bookRoute);
 mongoose.connect(mongoDBURL)
 .then(()=>{
   console.log("App connected to database");
